be859ea1b5dc69d4a07dfd46d4b6f5656c67b4f3
Lev Pasichnyi <lev.pasichnyi@gmail.com>
2022-07-16 20:32:56 +0300

    tips for wsl2 were added

---
+++ wsl2_tips_and_triks.sh
@@ -0,0 +1,13 @@
+# to open WSL2 file system from Windows
+
+# Just type in the Windows explorer: 
+\\wsl$\
+
+# to copy files from Windows in WSL terminal (between SSH folders as example):
+cp -r /mnt/c/Users/levpa/.ssh/ ~/.ssh/
+
+# to restart WSL2 just close - open terminal window or use these command from powershell
+wsl --shutdown
+
+# boot up distro with new terminal session
+wsl
